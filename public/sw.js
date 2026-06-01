91a6ff1d9ca3d591ad41ae72ba6530e0d7b7f6c1
uditt490-pixel <uditt490@gmail.com>
2026-06-01 18:10:17 +0530

    fix: increase tag scraper timeout and update zoneId
    Extend AI tag cleaner timeout to 15s to reduce failures and update service worker zoneId for accurate tracking.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11086143
+    "zoneId": 11086158
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
